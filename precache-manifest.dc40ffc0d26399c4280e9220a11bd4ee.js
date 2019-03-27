b400c6a8f8fa372093f5ff6ecc0e5391eed52ddb
Marc Marquez <marc.marquez@gmail.com>
2019-03-26 17:02:18 -0700

    Updates

---
+++ precache-manifest.dc40ffc0d26399c4280e9220a11bd4ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/burger-builder/static/media/burger-logo.b8503d26.png"
+  },
+  {
+    "revision": "6168d9880ae57e7f6b00",
+    "url": "/burger-builder/static/js/runtime~main.ad75294f.js"
+  },
+  {
+    "revision": "8596cfe92f1276d6eca8",
+    "url": "/burger-builder/static/js/main.ad7da157.chunk.js"
+  },
+  {
+    "revision": "23da151751114feeb4f0",
+    "url": "/burger-builder/static/js/2.c0c9e4aa.chunk.js"
+  },
+  {
+    "revision": "8596cfe92f1276d6eca8",
+    "url": "/burger-builder/static/css/main.991d946f.chunk.css"
+  },
+  {
+    "revision": "2968b176935e35afc072bc7987c75e2d",
+    "url": "/burger-builder/index.html"
+  }
+];
